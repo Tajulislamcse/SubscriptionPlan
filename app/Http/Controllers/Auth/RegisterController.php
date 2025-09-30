e9df20de940357bf8bd3f8f3d8cff2dd7ecd03e3
tajulislamcse <tajulislam8028@gmail.com>
2025-09-30 16:11:29 +0600

    Fix login and register form structure

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 class RegisterController extends Controller
 {
-    //
+    public function showRegisterForm()
+    {
+        return view('auth.register');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
 Route::get('/login',[LoginController::class,'showLoginForm'])->name('login');
 Route::post('/login',[LoginController::class,'login'])->name('login.submit');
+Route::get('/register',[RegisterController::class,'showRegisterForm'])->name('register');
