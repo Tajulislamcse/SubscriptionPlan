55b9f37e577ea8d2f9e71d0cc4524523c44366e8
tajulislamcse <tajulislam8028@gmail.com>
2025-09-30 19:15:28 +0600

    verified

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
-
+namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Events\Registered;
 use App\Http\Requests\RegisterRequest;
 
-class RegisterController extends Controller
+
+class AuthController extends Controller
 {
-    public function showRegisterForm()
+      public function showRegisterForm()
     {
         return view('auth.register');
     }
@@ -29,4 +30,21 @@ class RegisterController extends Controller
 
         return back()->with('success', 'Registration successful. Please check your email for verification.');
     }
+    public function showLoginForm()
+    {
+    
+        return view('auth.login');
+    }
+    public function emailVerify($id, $hash)
+    {
+        $user = User::findOrFail($id);
+        if (! hash_equals(sha1($user->getEmailForVerification()), $hash)) {
+            abort(403);
+        }
+        if (! $user->hasVerifiedEmail()) {
+            $user->markEmailAsVerified();
+        }
+        return redirect('/login')->with('verified', true);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,18 @@
 <?php
 
-use Illuminate\Auth\Events\Login;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::get('/login',[LoginController::class,'showLoginForm'])->name('login');
-Route::post('/login',[LoginController::class,'login'])->name('login.submit');
-Route::get('/register',[RegisterController::class,'showRegisterForm'])->name('register');
-Route::post('/register',[RegisterController::class,'register'])->name('register.submit');
+Route::get('/login',[AuthController::class,'showLoginForm'])->name('login');
+Route::post('/login',[AuthController::class,'login'])->name('login.submit');
+Route::get('/register',[AuthController::class,'showRegisterForm'])->name('register');
+Route::post('/register',[AuthController::class,'register'])->name('register.submit');
+Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'emailVerify'])->name('verification.verify')->middleware('signed');
 
-// Routes for authenticated users
-Route::middleware(['auth'])->group(function () {
-    
-    // Verification notice
-    Route::get('/email/verify', function () {
-        return view('auth.verify-email'); // create this blade file
-    })->name('verification.notice');
 
-    // Verify email
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-        return redirect('/dashboard'); // redirect after verification
-    })->middleware(['signed'])->name('verification.verify');
-
-    // Resend verification link
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-        return back()->with('message', 'Verification link sent!');
-    })->name('verification.send');
-});
 
